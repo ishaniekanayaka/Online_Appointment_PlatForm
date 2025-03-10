e3b71e087d89a1398c05538eb7acfeaf35d1604f
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-03-10 22:38:17 +0530

    first commit

---
+++ src/main/java/lk/ijse/online_appointment_platform/OnlineAppointmentPlatFormApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.online_appointment_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAppointmentPlatFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineAppointmentPlatFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/online_appointment_platform/OnlineAppointmentPlatFormApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.online_appointment_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAppointmentPlatFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
