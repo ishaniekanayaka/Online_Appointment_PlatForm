52581887b79637970be629230a9cfa12361a52f6
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-03-19 13:38:44 +0530

    categoryDTO done

--- src/main/java/lk/ijse/online_appointment_platform/dto/SubCategoryDTO.java
+++ src/main/java/lk/ijse/online_appointment_platform/dto/SubCategoryDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.online_appointment_platform.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class SubCategoryDTO {
+    private String id;
+    private String name;
+    private String description;
+    private String image;
+    private String categoryId;
+    private String categoryName;
 }
