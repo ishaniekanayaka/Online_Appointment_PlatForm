0327d64f88e09f85b07b0f911729ef0e54970c3a
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-03-30 14:48:48 +0530

    update appointment database

--- src/main/java/lk/ijse/online_appointment_platform/entity/Appointment.java
+++ src/main/java/lk/ijse/online_appointment_platform/entity/Appointment.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @AllArgsConstructor
@@ -15,10 +18,22 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
-    private String date;
-    private String time;
+    @Column(nullable = false)
+    private LocalDateTime date;  // Use LocalDateTime instead of Date for both date and time
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.date == null) {
+            this.date = LocalDateTime.now();  // Sets current date and time automatically
+        }
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "gig_id")  // Foreign key to Gig_details
+    private Gig_details gig;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;  // Use LocalDateTime for date and time
 
     @ManyToOne
     @JoinColumn(name = "user_id")
