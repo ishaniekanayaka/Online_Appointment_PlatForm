ee8efd0a02f3ccdfb2801e0f0b086ac499029e62
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-03-30 14:51:00 +0530

    create gig repository

--- src/main/java/lk/ijse/online_appointment_platform/repo/GigDetailsRepository.java
+++ src/main/java/lk/ijse/online_appointment_platform/repo/GigDetailsRepository.java
@@ -3,5 +3,8 @@ package lk.ijse.online_appointment_platform.repo;
 import lk.ijse.online_appointment_platform.entity.Gig_details;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GigDetailsRepository extends JpaRepository<Gig_details, Long> {
+    Optional<Gig_details> findById(Long gigId);
 }
