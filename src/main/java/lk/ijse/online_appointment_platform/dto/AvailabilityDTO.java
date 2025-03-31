68715f1473debd599b77813786c7598290c99896
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-03-31 19:29:52 +0530

    create AvailabilityDTO

---
+++ src/main/java/lk/ijse/online_appointment_platform/dto/AvailabilityDTO.java
@@ -0,0 +1,18 @@
+package lk.ijse.online_appointment_platform.dto;
+
+import lk.ijse.online_appointment_platform.enumClass.AvailabilityStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AvailabilityDTO {
+    private Long id;
+    private LocalDateTime dateTime;
+    private String userFullName;
+    private AvailabilityStatus status;
+}
