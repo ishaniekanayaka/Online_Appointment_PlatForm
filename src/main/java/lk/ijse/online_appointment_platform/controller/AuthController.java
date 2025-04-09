1f079174698a1f49f869351d9a3d64955901993c
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-04-09 10:56:23 +0530

    edit auth controller

--- src/main/java/lk/ijse/online_appointment_platform/controller/AuthController.java
+++ src/main/java/lk/ijse/online_appointment_platform/controller/AuthController.java
@@ -4,14 +4,20 @@ package lk.ijse.online_appointment_platform.controller;
 import lk.ijse.online_appointment_platform.dto.AuthDTO;
 import lk.ijse.online_appointment_platform.dto.ResponseDTO;
 import lk.ijse.online_appointment_platform.dto.UserDTO;
+import lk.ijse.online_appointment_platform.service.OTPService;
 import lk.ijse.online_appointment_platform.service.impl.UserServiceImpl;
 import lk.ijse.online_appointment_platform.util.JwtUtil;
 import lk.ijse.online_appointment_platform.util.VarList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -22,6 +28,9 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final UserServiceImpl userService;
     private final ResponseDTO responseDTO;
+
+    @Autowired
+    private OTPService otpService;
 
     //constructor injection
     public AuthController(JwtUtil jwtUtil, AuthenticationManager authenticationManager, UserServiceImpl userService, ResponseDTO responseDTO) {
@@ -74,7 +83,26 @@ public class AuthController {
         }
     }
 
+    @PostMapping("/send-otp")
+    public ResponseEntity<Map<String, String>> sendOtp(@RequestBody Map<String, String> request) throws MessagingException {
+        String email = request.get("email");
+        otpService.sendOtp(email);
+        return ResponseEntity.ok(Collections.singletonMap("message", "OTP sent."));
+    }
 
+    @PostMapping("/verify-otp")
+    public ResponseEntity<Map<String, String>> verifyOtp(@RequestBody Map<String, String> request) {
+        String email = request.get("email");
+        String otp = request.get("otp");
+        String newPassword = request.get("newPassword");
+
+        if (otpService.verifyOtp(email, otp, newPassword)) {
+            return ResponseEntity.ok(Collections.singletonMap("message", "Password updated."));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", "Invalid OTP or expired."));
+        }
+
+    }
 
 
 }
