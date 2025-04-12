4520b2e33b1556661e7fb5129004476600aeb036
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-04-12 13:41:57 +0530

    update gig details repo l

--- src/main/java/lk/ijse/online_appointment_platform/repo/GigDetailsRepository.java
+++ src/main/java/lk/ijse/online_appointment_platform/repo/GigDetailsRepository.java
@@ -4,6 +4,7 @@ import lk.ijse.online_appointment_platform.dto.GigDetailsResponseDTO;
 import lk.ijse.online_appointment_platform.entity.Gig_details;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,11 +14,18 @@ public interface GigDetailsRepository extends JpaRepository<Gig_details, Long> {
     List<Gig_details> findBySubCategoryId(Long subCategoryId);
     List<Gig_details> findByUserId(Long userId);
 
+//    @Query("SELECT new lk.ijse.online_appointment_platform.dto.GigDetailsResponseDTO(" +
+//            "g.id, g.FullName, g.image, g.degree, g.experience, g.amountCharge, " +
+//            "g.location, g.contactNumber, g.maxAppointmentsPerDay, g.active, g.dateTime, " +
+//            "g.user.id, g.category.name, g.subCategory.name) " +
+//            "FROM Gig_details g")
+//    List<GigDetailsResponseDTO> getAllGigDetailsWithNames();
+
     @Query("SELECT new lk.ijse.online_appointment_platform.dto.GigDetailsResponseDTO(" +
             "g.id, g.FullName, g.image, g.degree, g.experience, g.amountCharge, " +
             "g.location, g.contactNumber, g.maxAppointmentsPerDay, g.active, g.dateTime, " +
             "g.user.id, g.category.name, g.subCategory.name) " +
-            "FROM Gig_details g")
-    List<GigDetailsResponseDTO> getAllGigDetailsWithNames();
+            "FROM Gig_details g WHERE g.id = :gigId")
+    Optional<GigDetailsResponseDTO> getGigDetailsByGigId(@Param("gigId") Long gigId);
 
 }
