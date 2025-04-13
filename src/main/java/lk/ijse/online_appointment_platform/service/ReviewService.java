11c6adf19fe5bb1e2bfed56acea5d2812e32ab34
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-04-13 10:27:21 +0530

    reviewService save review done

---
+++ src/main/java/lk/ijse/online_appointment_platform/service/ReviewService.java
@@ -0,0 +1,7 @@
+package lk.ijse.online_appointment_platform.service;
+
+import lk.ijse.online_appointment_platform.dto.ReviewDTO;
+
+public interface ReviewService {
+    public void saveReview(ReviewDTO dto);
+}
