de9d05415a35fcf5e5af7dc521c30bb1b6ab5330
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2025-04-13 10:13:29 +0530

    create reviewDTO

---
+++ src/main/java/lk/ijse/online_appointment_platform/dto/ReviewDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.online_appointment_platform.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ReviewDTO {
+    private Long userId;
+    private Long gigId;
+}
